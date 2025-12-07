79d91f83d1017a30a1569fa680e24c45e2d23ba1
Aayush Raj <aayushr679@gmail.com>
2025-12-07 23:34:03 +0530

    Create worst_fit.c

---
+++ 11_Memory_Allocation/worst_fit.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+int main() {
+    int blocks[20], processes[20], allocation[20];
+    int nb, np;
+
+    printf("Enter number of memory blocks: ");
+    scanf("%d", &nb);
+
+    printf("Enter block sizes:\n");
+    for (int i = 0; i < nb; i++) scanf("%d", &blocks[i]);
+
+    printf("Enter number of processes: ");
+    scanf("%d", &np);
+
+    printf("Enter process sizes:\n");
+    for (int i = 0; i < np; i++) scanf("%d", &processes[i]);
+
+    for (int i = 0; i < np; i++) allocation[i] = -1;
+
+    for (int i = 0; i < np; i++) {
+        int worst = -1;
+        for (int j = 0; j < nb; j++) {
+            if (blocks[j] >= processes[i]) {
+                if (worst == -1 || blocks[j] > blocks[worst]) {
+                    worst = j;
+                }
+            }
+        }
+
+        if (worst != -1) {
+            allocation[i] = worst;
+            blocks[worst] -= processes[i];
+        }
+    }
+
+    printf("\nProcess No.\tProcess Size\tBlock Allocated\n");
+    for (int i = 0; i < np; i++) {
+        printf("%d\t\t%d\t\t", i+1, processes[i]);
+        if (allocation[i] != -1)
+            printf("%d\n", allocation[i] + 1);
+        else
+            printf("Not Allocated\n");
+    }
+
+    return 0;
+}
